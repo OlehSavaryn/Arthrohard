c253fa5842deb8cf764c810aa5bae9d6d2b3e06e
Oleh Savaryn <olegsavaryn@gmail.com>
2024-12-02 18:44:51 +0100

    hero,section1,section2

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 // Burger Function
 
+const body = document.querySelector("body");
 const burgerMenu = document.querySelector(".burger-menu");
 const burgerBtn = document.querySelector(".burger-btn");
 const mobNavItem = document.querySelectorAll(".burger-menu");
@@ -7,11 +8,13 @@ const mobNavItem = document.querySelectorAll(".burger-menu");
 function toggleMenu() {
   if (burgerMenu.classList.contains("show-menu")) {
     burgerMenu.classList.remove("show-menu");
+    body.classList.remove("overflow-hidden");
     mobNavItem.forEach(function (menuItem) {
       menuItem.addEventListener("click", toggleMenu);
     });
   } else {
     burgerMenu.classList.add("show-menu");
+    body.classList.add("overflow-hidden");
     mobNavItem.forEach(function (menuItem) {
       menuItem.addEventListener("click", toggleMenu);
     });
